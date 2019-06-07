9ae6815d8b747f8ca8d0d66514cae4f9c8f480ef
Warren Yao <warrenyca905@gmail.com>
2019-06-07 02:58:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,7 @@ class Transfer
         receiver.deposit(amount)
         self.status = "complete"
       else
+        self.status = "rejected"
         return "Transaction rejected. Please check your account balance."
       end
     end
