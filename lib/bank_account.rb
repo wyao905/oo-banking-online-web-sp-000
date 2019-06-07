b2e1d6cc8f7be64143b962c6d9ca29d3efbf390e
Warren Yao <warrenyca905@gmail.com>
2019-06-07 01:40:33 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,20 @@
-class BankAccount
+require 'pry'
 
+class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+    binding.pry
+    balance = balance + amount
+  end
+  
+  # def display_balance
+    
 end
