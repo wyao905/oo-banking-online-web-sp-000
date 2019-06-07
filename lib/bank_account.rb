f1968dcd3be713e2fa63dc12172d83e596ab2d82
Warren Yao <warrenyca905@gmail.com>
2019-06-07 02:31:20 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -15,4 +15,12 @@ class BankAccount
   def display_balance
     "Your balance is $#{balance}."
   end
+  
+  def valid?
+    if status == "open"
+      true
+    else
+      false
+    end
+  end
 end
